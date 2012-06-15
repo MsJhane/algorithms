ef46de5f4b731ef2e97390792ee947ab7db456d7
MsJhane <jhane_alambra@yahoo.com>
2012-06-15 14:09:19 +0800

    new program

---
+++ queue1.py
@@ -0,0 +1,11 @@
+aList=["Mr.A","Mr.C","Mr.B"]
+i=1
+for person in aList:
+    print("#%i : %s " % (i,person))
+    i=i+1
+
+Head=("%s" % aList[0])
+Tail=("%s" % aList [len(aList)-1])
+print("Head:" + Head)
+print("Tail:" + Tail)
+
